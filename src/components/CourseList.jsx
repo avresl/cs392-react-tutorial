84d17ab42db9900453bd0a500187d518975112ae
avresl <avre.fla@gmail.com>
2023-10-01 17:59:37 -0500

    fetch-data

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,17 +1,17 @@
 import './CourseList.css'
 
 const CourseList = ({courses}) => (
-        <div className="course-list">
-            { Object.values(courses).map(id => (
-                <div className="course-card" key={id}>
-                    <div className="course-term-number"> {id.term} CS {id.number} </div>
-                    <div className='course-card-top'>
-                        {id.title}
-                    </div>
-                    <div className='course-card-bottom'>{id.meets}</div>
+    <div className="course-list">
+        { Object.values(courses).map(id => (
+            <div className="course-card" key={id}>
+                <div className="course-term-number"> {id.term} CS {id.number} </div>
+                <div className='course-card-top'>
+                    {id.title}
                 </div>
-            ))}
-        </div>
+                <div className='course-card-bottom'>{id.meets}</div>
+            </div>
+        ))}
+    </div>
 );
 
 export default CourseList;
