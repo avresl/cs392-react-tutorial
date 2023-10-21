a430b23584e6f732c93b0c789875c797c7a2afeb
avresl <avre.fla@gmail.com>
2023-10-21 16:26:19 -0500

    add-authentication fixed

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,6 +2,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import './CourseList.css'
 import { isConflicting } from '../utilities/time-filter';
 import { Link } from 'react-router-dom';
+import { useAuthState } from '../utilities/firebase';
 
 const CourseList = ({courses, selection, selectedCourses, setSelectedCourses}) => {
 
@@ -13,6 +14,15 @@ const CourseList = ({courses, selection, selectedCourses, setSelectedCourses}) =
 
     const isSelected = (item) => (selectedCourses.includes(item));
 
+    const editButton = (term, number) => {
+        const [user] = useAuthState();
+        if(user) {
+            return (<Link className="ms-auto" to={`/${term.substring(0,1)+number}/edit`}><i className="bi bi-pencil"></i></Link>);
+        } else {
+            return (<></>);
+        }
+    }
+
     return (
         <div className="course-list">
             { Object.values(courses).filter(item => (item.term == selection)).map(id => (
@@ -23,7 +33,7 @@ const CourseList = ({courses, selection, selectedCourses, setSelectedCourses}) =
                 >
                     <div className='d-flex'>
                         <div className="me-auto course-term-number"> {id.term} CS {id.number}</div>
-                        <Link className="ms-auto" to={`/${id.term.substring(0,1)+id.number}/edit`}><i className="bi bi-pencil"></i></Link>
+                        { editButton(id.term, id.number) }
                     </div>
                     <div className='course-card-top'>
                         {id.title}
